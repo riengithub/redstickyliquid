859ad3145e24c1113b757fc930d7d4eab0723190
user1 <user1@DESKTOP-2K6R2B7>
2024-05-22 18:16:13 +0900

    [INIT] 별찍기 22
    개힘들었다

--- src/org/redstickyliquid/day4/control/loop/exercise/printStars22.java
+++ src/org/redstickyliquid/day4/control/loop/exercise/printStars22.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class printStars22 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.print("아무 양의 정수나 입력하세요: ");
 		int n = sc.nextInt();
 		int l = 2 * (2 * n - 1);
 		int x = l - 1;
@@ -13,11 +14,11 @@ public class printStars22 {
 		String[][] stars = new String[y][x];
 
 		for(int i = 0, j = x - 1, direction = 0, margin = 0, turn = 1;
-				turn <= x;
+				turn <= l;
 						direction = direction > 3 ? 0 : direction,
 						j += direction == 0 ? -1 : direction == 2 ? 1 : 0,
 						i += direction == 1 ? 1 : direction == 3 ? -1 : 0,
-						margin = turn >= 4 ? (turn/4) : 0) {
+						margin = turn >= 4 ? (turn/4)*2 : 0) {
 			
 			if (direction == 0 && (j > 0 + margin)
 					|| direction == 1 && (i < y - 1 - margin)
@@ -26,33 +27,29 @@ public class printStars22 {
 					) {
 				
 						stars[i][j] = "*";
-						System.out.println("coordinates: "+j+", "+i+ " "+stars[i][j]+" - Margin and direction :" +margin + ", " +  direction);
+//						System.out.println("coordinates: "+j+", "+i+ " "+stars[i][j]+" - Margin and direction :" +margin + ", " +  direction);
 					} else {
 						stars[i][j] = "*";
-						System.out.println("coordinates: "+j+", "+i+ " "+stars[i][j]+" - Margin and direction :" +margin + ", " +  direction);
-						System.out.println("turning");
+//						System.out.println("coordinates: "+j+", "+i+ " "+stars[i][j]+" - Margin and direction :" +margin + ", " +  direction);
+//						System.out.println("turning");
 						direction++; turn++;
 					};
-					
-					for (int a = 0; a < y; a++) {
-						for (int b = 0; b < x; b++) {
-								if (stars[a][b] == null) System.out.print(" "); else System.out.print(stars[a][b]);
-							}
-						System.out.println("");
-					}
-					
-					
-			
+//					for (int a = 0; a < y; a++) {
+//						for (int b = 0; b < x; b++) {
+//								if (stars[a][b] == null) System.out.print(" "); else System.out.print(stars[a][b]);
+//							}
+////						System.out.println("");
+//					}
 		}
 		
 		sc.close();
 
-//		for (int i = 0; i < y; i++) {
-//			for (int j = 0; j < x; j++) {
-//					if (stars[i][j] == null) System.out.print(" "); else System.out.print(stars[i][j]);
-//				}
-//			System.out.println("");
-//		}
+		for (int i = 0; i < y; i++) {
+			for (int j = 0; j < x; j++) {
+					if (stars[i][j] == null) System.out.print(" "); else System.out.print(stars[i][j]);
+				}
+			System.out.println("");
+		}
 		
 	}
 }
